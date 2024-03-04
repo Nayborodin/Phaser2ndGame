2ed8a8c73140b35a6f463e2ebac151fab77d1bbe
Nayborodin <158287255+Nayborodin@users.noreply.github.com>
2024-03-04 12:31:17 +0200

    додали картинки

---
+++ game.js
@@ -0,0 +1,10 @@
+function preload ()
+{
+    this.load.image('crate','assets/Object/Crate.png');
+    this.load.image('icebox','assets/Object/IceBox.png');
+    this.load.image('stone','assets/Object/Stone.png');
+    this.load.image('tree1','assets/Object/Tree_1');
+    this.load.image('tree2','assets/Object/Tree_2');
+    this.load.image('igloo','assets/Object/Igloo');
+    this.load.image('background','assets/background');
+}
