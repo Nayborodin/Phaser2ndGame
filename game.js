baa21ced7c6530deb6a1ba0d3a135f58cdc88eb8
Nayborodin <158287255+Nayborodin@users.noreply.github.com>
2024-03-04 12:36:53 +0200

    додали ширину та висоту гри та гравітацію

--- game.js
+++ game.js
@@ -1,3 +1,20 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 function preload ()
 {
     this.load.image('crate','assets/Object/Crate.png');
