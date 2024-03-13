a6d19c8b195bd2122d761148ab68edfb02296426
Nayborodin <158287255+Nayborodin@users.noreply.github.com>
2024-03-13 14:23:31 +0200

    добавили фон і платформу

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ var config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 300 },
-            debug: true
+            debug: false
         }
     },
     scene: {
@@ -35,35 +35,34 @@ function preload ()
     this.load.image('tree1','assets/Object/Tree_1.png');
     this.load.image('tree2','assets/Object/Tree_2.png');
     this.load.image('igloo','assets/Object/Igloo.png');
-    this.load.image('background','assets/2.png');
+    this.load.image('background','assets/2.jpg');
     this.load.image('star','assets/star.png');
     this.load.image('platform','assets/platform.png');
     this.load.image('14','assets/Tiles/14.png');
     this.load.image('15','assets/Tiles/15.png');
     this.load.image('16','assets/Tiles/16.png');
+    this.load.image('2','assets/Tiles/2.png');
 }
 function create ()
 {
-    this.add.image(600, 500, 'background');
-//налаштування камери
-    this.cameras.main.setBounds(0, 0, worldWidth, 1080);
-    this.physics.world.setBounce(0, 0, worldWidth, 1080);
-//камера слідкує за гравцем
-    this.cameras.main.startFollow(player);
+    this.add.image(1000, 540, 'background');
+    //створюємо платформи
+platforms = this.physics.add.staticGroup();
+    for (var x = 0; x < worldWidth; x = x + 128)
+    {
+        console.log(x)
+        platforms
+        .create(x , 1080 - 128, '2')
+        .setOrigin(0,0)
+        .refreshBody();
+    }
+
 
     this.add.tileSprite(0,0,worldWidth, 1080,"background")
     .setOrigin(0,0)
     .setScale(1)
     .setDepth(5);
-    platforms = this.physics.add.staticGroup();
-    for (var x = 0; 0 < worldWidth; x = x + 128)
-    {
-        platforms
-        .create(x, 1080 - 128,'platform')
-        .setOrigin(0,0)
-        .refreshBody();
-    }
-    //платформи в повітрі
+//платформи в повітрі
     for (var x=0; x<worldWidth; x=x+Phaser.Math.FloatBetween(256,500))
     {
       var y=Phaser.Math.Beetween(128,810)
@@ -75,14 +74,22 @@ function create ()
       }
       platforms.create(x + 128 * i, y, '16')
     }
+//створюємо гравця
+    player = this.physics.add.sprite(800, 800, 'dude')
+    player
+    .setBounds(0,2)
+    .setCollideWorldBonuds(true)
+    /setDepth(5);
+    
+    this.physics.add.collider(player,platforms);
+//налаштування камери
+    this.cameras.main.setBounds(0, 0, worldWidth, 1080);
+    this.physics.world.setBounds(0, 0, worldWidth, 1080);
+//камера слідкує за гравцем
+    this.cameras.main.startFollow(player);
 }
 function update ()
 {
-    if (gameOver)
-    {
-        return;
-    }
-
     if (cursors.left.isDown)
     {
         player.setVelocityX(-playerSpeed);
