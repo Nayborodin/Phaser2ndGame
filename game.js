79afb6ff2dd907061afc682bfeceace728a092fd
Nayborodin <158287255+Nayborodin@users.noreply.github.com>
2024-03-18 12:51:28 +0200

    добавили платформи,дерево,зірки та гравця

--- game.js
+++ game.js
@@ -26,6 +26,8 @@ var gameOver=false;
 var worldWidth=config.width*10;
 var star;
 var cursors;
+var tree1;
+var tree2;
 function preload ()
 {
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 90, frameHeight: 90 });
@@ -57,29 +59,86 @@ platforms = this.physics.add.staticGroup();
         .refreshBody();
     }
     //платформи в повітрі
-    platforms = this.physics.aa.staticGroup();
+    platforms = this.physics.add.staticGroup();
 
-    for(var x =0; x <= worldWidth; x = x + 128)
-    {
-        platforms.create(x,1080 - 128, '14').setOrigin(0,0).refreshBody(); 
+    for(var x =0; x <= worldWidth; x = x + Phaser.Math.Between(256,500)){
+        var y = Phaser.Math.Between(128,810)
+
+        platforms.create(x, y, '14')
+        var i
+        for(i = 1; i<=Phaser.Math.Between(1,5);i++){
+            platforms.create(x + 128 * i, y, '15')
+        }
+
+        platforms.create(x + 128 * i, y, '16')
     }
-    //object = this.physics.add.staticGroup();
-    //function createWorldObjects(Object, assets)
+    //створюємо гравця
+    //player = this.physics.add.staticGroup();
+    player = this.physics.add.sprite(800, 800, 'dude');
+    player
+    .setBounce(0.2)
+    .setCollideWorldBounds(false)
+    .setDepth(5);
+    //колізія гравця та платформи
+    this.physics.add.collider(player, platforms);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    cursors = this.input.keyboard.createCursorKeys();
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 111,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
     
-    //for (var x = 0; x <= worldWidth; x = x + Phaser.Math.FloatBetween(256,500))
+    //додаємо об'єкти випадковим чином
+    tree1 = this.physics.add.staticGroup();
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(1000,1500))
+    {
+        var y = Phaser.Math.FloatBetween(100,1000)
+        console.log(x, y)
+        tree1
+        .create(x, 1080 - 128, 'tree1')
+        .setOrigin(0, 1)
+        .setScale(Phaser.Math.FloatBetween(0.8, 1.5))
+        .setDepth(Phaser.Math.Between(1, 10))
+        console.log(tree1,X, tree1,Y)
+    }
     
 }
 function update ()
 {
+    if (gameOver)
+    {
+        return;
+    }
+
     if (cursors.left.isDown)
     {
-        player.setVelocityX(-playerSpeed);
+        player.setVelocityX(-160);
 
         player.anims.play('left', true);
     }
     else if (cursors.right.isDown)
     {
-        player.setVelocityX(playerSpeed);
+        player.setVelocityX(160);
 
         player.anims.play('right', true);
     }
@@ -94,5 +153,4 @@ function update ()
     {
         player.setVelocityY(-330);
     }
-
 }
